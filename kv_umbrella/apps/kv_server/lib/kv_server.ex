860323e1873bbad8ef3f986535b2fb0538e3f81b
Enrico lerrigatto La Cava <me@lerrigatto.com>
2021-02-20 10:15:01 +0100

    kvserver: add rudimental echo server

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,40 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KVServer.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def accept(port) do
+    
+    # The following line open a tcp listener with the following options:
+    # `:binary` - receives data as binaries, instead of lists
+    # `packet: :line` - receives data live by line
+    # `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # `reuseaddr: true` - reuse an address if the listener crashes
+    #
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
   end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
+
 end
