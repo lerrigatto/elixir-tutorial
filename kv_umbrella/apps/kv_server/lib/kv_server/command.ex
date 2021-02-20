19f0dfdc569d802c3fc4f0be61a101c773ff388c
Enrico lerrigatto La Cava <me@lerrigatto.com>
2021-02-20 11:54:31 +0100

    kvserver: add doctest for common operations

--- kv_umbrella/apps/kv_server/lib/kv_server/command.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/command.ex
@@ -3,9 +3,33 @@ defmodule KVServer.Command do
   Parses the given `line` into a command.
 
   ## Examples
-  
+
+  You can create a bucket
     iex> KVServer.Command.parse("CREATE shopping\r\n")
     {:ok, {:create, "shopping"}}
+
+  Trailing spaces won't matter
+    iex> KVServer.Command.parse("CREATE   shopping  \r\n")
+    {:ok, {:create, "shopping"}}
+
+  You can put stuff in a bucket
+    iex> KVServer.Command.parse("PUT shopping milk 2 \r\n")
+    {:ok, {:put, "shopping", "milk", "2"}}
+
+  You can retrieve the stuff you put
+    iex> KVServer.Command.parse("GET shopping milk \r\n")
+    {:ok, {:get, "shopping", "milk"}}
+
+  You can delete stuff in a bucket
+    iex> KVServer.Command.parse("DELETE shopping eggs \r\n")
+    {:ok, {:delete, "shopping", "eggs"}}
+
+  Unknown commands or commands with the wrong number of arguments will return an error:
+    iex> KVServer.Command.parse("UNKNOWN shopping stuff \r\n")
+    {:error, :unknown_command}
+
+    iex> KVServer.Command.parse("GET shopping \r\n")
+    {:error, :unknown_command}
 
   """
   def parse(line) do
